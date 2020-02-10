9f2fbe0f854ab78644d176cfed3745e5ea51278d
clarahzz <57667851+clarahzz@users.noreply.github.com>
2020-02-10 19:31:50 -0300

    Update complementos

--- complementos.py
+++ complementos.py
@@ -5,24 +5,15 @@
 # Ejemplo parseo argumentos
 
 # CONSTANTES
-G = 0.1
-AREA = 10000
-ca = 10  # Constante que se usa para la fuerza de atracción
-C = 0.95  # Constante que se usa para la temperatura
-CLOSEZERO = 0.00001
-EPSILON = 0.005
-G = 0.1  # Constante de gravedad
+
 
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
-from math import sqrt, pow
+from math import sqrt
 from random import uniform
 
 
-def mul_escalar(vector, e):
-    v1 = (vector[0] * e, vector[1] * e)
-    return v1
 
 class LayoutGraph:
 
@@ -38,6 +29,8 @@ class LayoutGraph:
 
         # Guardo el grafo
         self.grafo = grafo
+        self.vertices = grafo[0]
+        self.aristas = grafo[1]
 
         # Inicializo estado
         # Completar
@@ -47,6 +40,9 @@ class LayoutGraph:
         self.acum_x = {}
         self.acum_y = {}
         self.t = 1
+        self.ancho = 10000
+        self.epsilon = 0.005
+        self.g = 0.1  # Constante de gravedad
 
         # Guardo opciones
         self.iters = iters
@@ -56,39 +52,38 @@ class LayoutGraph:
         self.c1 = c1
         self.c2 = c2
 
+
     def posiciones_random(self):
-        vertices = self.grafo[0]
-        for vertice in vertices:
-            self.posicion_x[vertice] = uniform(0, AREA)
-            self.posicion_y[vertice] = uniform(0, AREA)
+        for vertice in self.vertices:
+            self.posicion_x[vertice] = uniform(0, self.ancho)
+            self.posicion_y[vertice] = uniform(0, self.ancho)
 
     def distancia(self, vertice0, vertice1):
-        d = sqrt(pow(self.posicion_x[vertice0] - self.posicion_x[vertice1], 2) + pow(
-            self.posicion_y[vertice0] - self.posicion_y[vertice1], 2))
+        d = sqrt((self.posicion_x[vertice0] - self.posicion_x[vertice1])**2 + (self.posicion_y[vertice0] - self.posicion_y[vertice1])** 2)
         return d
 
-    def fuerza_atraccion(self, d, vertices):
-        k = ca * sqrt(AREA / len(vertices))
-        f = pow(d, 2) / k
+    def mul_escalar(self, vector, e):
+        v1 = (vector[0] * e, vector[1] * e)
+        return v1
+
+    def fuerza_atraccion(self, d):
+        k = self.c2 * sqrt(self.ancho*self.ancho / len(self.vertices))
+        f = d**2 / k
         return f
 
-    def fuerza_repulsion(self, d, vertices):
-        k = ca * sqrt(AREA / len(vertices))
-        f = pow(k, 2) / d
-        return f
+    def fuerza_repulsion(self, d):  #Está bien
+        k = self.c1 * sqrt(self.ancho*self.ancho / len(self.vertices))
+        return k**2 / d
 
-    def inicializar_acumuladores(self):
-        vertices = self.grafo[0]
-        for vertice in vertices:
+    def inicializar_acumuladores(self): #Está bien
+        for vertice in self.vertices:
             self.acum_x[vertice] = 0
             self.acum_y[vertice] = 0
 
-    def calcular_fuerza_atraccion(self):
-
-        aristas = self.grafo[1]
-        for arista in aristas:
+    def calcular_fuerza_atraccion(self):  #Está bien
+        for arista in self.aristas:
             d = self.distancia(arista[0], arista[1])
-            mod_fa = self.fuerza_atraccion(d, self.grafo[0])
+            mod_fa = self.fuerza_atraccion(d)
 
             # Consideramos el caso de divisiones por 0
             while (d < 0):
@@ -99,76 +94,76 @@ class LayoutGraph:
                 self.posicion_y[arista[1]] -= f
                 d = dist(arista[0], arista[1])
 
-            mod_fa = self.fuerza_atraccion(d, self.grafo[0])
-            fx = ((mod_fa) * (self.posicion_x[arista[0]] - self.posicion_x[arista[1]]))
-            fy = ((mod_fa) * (self.posicion_y[arista[0]] - self.posicion_y[arista[1]]))
+            mod_fa = self.fuerza_atraccion(d)
+            fx = (mod_fa * (self.posicion_x[arista[1]] - self.posicion_x[arista[0]])) / d
+            fy = (mod_fa * (self.posicion_y[arista[1]] - self.posicion_y[arista[0]])) / d
 
             self.acum_x[arista[0]] += fx
             self.acum_y[arista[0]] += fy
             self.acum_x[arista[1]] -= fx
             self.acum_y[arista[1]] -= fy
 
-    def calcular_fuerza_repulsion(self):
-        vertices = self.grafo[0]
-        for i in range(0, len(vertices)):
-            for j in range(i + 1, len(vertices)):
-                vertice1 = vertices[i]
-                vertice2 = vertices[j]
-                d = self.distancia(vertice1, vertice2)
+    def calcular_fuerza_repulsion(self): #Está bien
+        for vertice1 in self.vertices:
+            for vertice2 in self.vertices:
+                    if vertice1 != vertice2:
+                        d = self.distancia(vertice1, vertice2)
 
-                # Consideramos el caso de divisiones por 0
-                while (d < 0):
-                    f = random.random()
-                    self.posicion_x[vertice1] += f
-                    self.posicion_y[vertice1] += f
-                    self.posicion_x[vertice2] -= f
-                    self.posicion_y[vertice2] -= f
-                    d = dist(vertice1, vertice2)
+                        # Consideramos el caso de divisiones por 0
+                        while (d < self.epsilon):
+                            f = random.random()
+                            self.posicion_x[vertice1] += f
+                            self.posicion_y[vertice1] += f
+                            self.posicion_x[vertice2] -= f
+                            self.posicion_y[vertice2] -= f
+                            d = distancia(vertice1, vertice2)
 
-                mod_fa = self.fuerza_repulsion(d, self.grafo[0])
-                fx = ((mod_fa) * (self.posicion_x[vertice1] - self.posicion_x[vertice2]))
-                fy = ((mod_fa) * (self.posicion_y[vertice1] - self.posicion_y[vertice2]))
+                        mod_fa = self.fuerza_repulsion(d)
+                        fx = (mod_fa * (self.posicion_x[vertice2] - self.posicion_x[vertice1])) / d
+                        fy = (mod_fa * (self.posicion_y[vertice2] - self.posicion_y[vertice1])) / d
 
-                self.acum_x[vertice1] -= fx
-                self.acum_y[vertice1] -= fy
-                self.acum_x[vertice2] += fx
-                self.acum_y[vertice2] += fy
+                        self.acum_x[vertice1] -= fx
+                        self.acum_y[vertice1] -= fy
+                        self.acum_x[vertice2] += fx
+                        self.acum_y[vertice2] += fy
 
 
 
-    def actualizar_posiciones(self):
-        vertices = self.grafo[0]
-        for vertice in vertices:
+    def actualizar_posiciones(self):  #Está bien, pero pensar si funciona cuando se va de los límites de la pantalla.
+        for vertice in self.vertices:
 
             f = (self.acum_x[vertice], self.acum_y[vertice])
-            modulo_f = sqrt(pow(f[0], 2) + pow(f[1], 2))
+            modulo_f = sqrt(f[0]**2 + f[1]**2)
             if modulo_f > self.t:
-                f = mul_escalar(mul_escalar(f, 1 / modulo_f), self.t)
+                f = (f[0] / modulo_f * self.t , f[1] / modulo_f * self.t)
                 (self.acum_x[vertice], self.acum_y[vertice]) = f
-            posicion_x = self.posicion_x[vertice] + self.acum_x[vertice]
-            posicion_y = self.posicion_y[vertice] + self.acum_y[vertice]
 
-            if posicion_x < AREA:
-                self.posicion_x[vertice] = posicion_x
-            else:
-                self.posicion_x[vertice] = AREA
-            if posicion_y < AREA:
-                self.posicion_y[vertice] = posicion_y
-            else:
-                self.posicion_y[vertice] = AREA
+            self.posicion_x[vertice] = self.posicion_x[vertice] + self.acum_x[vertice]
+            self.posicion_y[vertice] = self.posicion_y[vertice] + self.acum_y[vertice]
 
-    def calcular_fuerza_gravedad(self):
-        pos_x = sqrt(AREA) / 2
-        pos_y = pos_x
-        for vertice in self.grafo[0]:
-            dist = sqrt(pow((pos_x - self.posicion_x[vertice]), 2) + (pow((pos_y - self.posicion_y[vertice]), 2)))
-            fx = ((G * (pos_x - self.posicion_x[vertice])) / dist)
-            fy = ((G * (pos_y - self.posicion_y[vertice])) / dist)
-            self.acum_x[vertice] -= fx
-            self.acum_y[vertice] -= fy
 
-    def actualizar_temperatura(self):
-        self.t = self.t * C  # Multiplicamos por la constante definida.
+
+    def calcular_fuerza_gravedad(self):  #Está bien.
+        centro = self.ancho / 2
+        for vertice in self.vertices:
+
+            d = sqrt((self.posicion_x[vertice] - centro)**2 + (self.posicion_y[vertice] - centro)** 2)
+
+            #Consideramos el caso de divisiones por 0
+            while d < self.epsilon:
+                while (d < self.epsilon):
+                    f = random.random()
+                    self.posicion_x[vertice] += f
+                    self.posicion_y[vertice] += f
+                    d = sqrt((self.posicion_x[vertice] - centro)**2 + (self.posicion_y[vertice] - centro)** 2)
+
+                fx = ((self.g * (pos_x - self.posicion_x[vertice])) / d)
+                fy = ((self.g * (pos_y - self.posicion_y[vertice])) / d)
+                self.acum_x[vertice] -= fx
+                self.acum_y[vertice] -= fy
+
+    def actualizar_temperatura(self):  #Está bien.
+        self.t = self.t * self.g  # Multiplicamos por la constante definida.
 
     def step(self):
         self.inicializar_acumuladores()
@@ -179,18 +174,18 @@ class LayoutGraph:
         self.actualizar_temperatura()
 
     def show_graph(self):
-        plt.pause(0.001)
-        posx = [self.posicion_x[i] for i in self.grafo[0]]
-        posy = [self.posicion_y[i] for i in self.grafo[0]]
+        plt.pause(0.0001)
+        x = [self.posicion_x[i] for i in self.grafo[0]]
+        y = [self.posicion_y[i] for i in self.grafo[0]]
         plt.clf()
 
-        plt.scatter(posx, posy)  # dibuja los puntos.
+        plt.scatter(x, y)  # dibuja los puntos.
 
-        for arista in self.grafo[1]:
+        for arista in self.aristas:
             vertice1 = arista[0]
             vertice2 = arista[1]
-            plt.plot([self.posicion_x[vertice1], self.posicion_x[vertice2]],
-                     [self.posicion_y[vertice1], self.posicion_y[vertice2]])
+            plt.plot((self.posicion_x[vertice1], self.posicion_x[vertice2]),
+                     (self.posicion_y[vertice1], self.posicion_y[vertice2]))
 
     def layout(self):
         '''
@@ -198,18 +193,15 @@ class LayoutGraph:
         un layout
         '''
         self.posiciones_random()
-        aristas = self.grafo[1]
         plt.ion()
-
         for i in range(self.iters):
-            self.step()
-            if self.refresh != 0:
-                self.show_graph()
+            if self.refresh != 0 and i % self.refresh == 0:  #Ya que imprime cada determinadas iteraciones.
+                    self.step()
+                    self.show_graph()
         if self.refresh == 0:
             self.show_graph()
-        plt.show  # lo muestra
         plt.ioff()  # lo cierra
-
+        plt.show()  # lo muestra
 
 def main():
     # Definimos los argumentos de linea de comando que aceptamos
